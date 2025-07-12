d0634c677eea7777cd8be090b44936d842ce8967
Carlogh2001 <carlomolko@gmail.com>
2025-07-12 20:13:53 +0000

    Clase

--- src/app.py
+++ src/app.py
@@ -1,10 +1,34 @@
 import os
 import pandas as pd
-import matplotlib.pyplot as plt
+import seaborn as sns
 from dotenv import load_dotenv
 
 # load the .env file variables
 load_dotenv()
 
+# Get credential values
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
 
-# Spotify API credentials
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+spotify = spotipy.Spotify(auth_manager=auth_manager)
+
+# 168dgYui7ExaU612eooDF1 Brand new
+
+# Obtener las mejores canciones del artista
+results= spotify.artist_top_tracks ("168dgYui7ExaU612eooDF1")
+
+
+songs = []
+
+for track in results ['tracks'] :
+    songs.append({"name":track['name'],
+                  "popularity":track['popularity'],
+                  "duracion_min":track['duration_ms']/60000})
+df= pd.DataFrame (songs)    
+
+print(df)
+
